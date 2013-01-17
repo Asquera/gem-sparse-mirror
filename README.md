e11b1886c11656bbe6bb58d1c49a094e32fbaf9f
Florian Gilcher <florian.gilcher@asquera.de>
2013-01-17 22:27:04 +0100

    Add a README, fix code style

--- README.md
+++ README.md
@@ -1,24 +1,44 @@
-# Gem::Sparse::Mirror
+# gem-sparse-mirror - Mirror parts of Rubygems
 
-TODO: Write a gem description
+A script that extends `rubygems-mirror` to download only parts of Rubygems. It uses the bundler API to resolve all dependencies of the intended gems and completely mirrors those.
 
 ## Installation
 
-Add this line to your application's Gemfile:
-
-    gem 'gem-sparse-mirror'
-
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
+Run:
 
     $ gem install gem-sparse-mirror
 
 ## Usage
 
-TODO: Write usage instructions here
+Like [https://github.com/rubygems/rubygems-mirror](rubygems-mirror), this reads `.gems/mirrorrc`, with 2 additional attributes, `only` and `except`:
+
+```yaml
+---
+- from: http://rubygems.org/
+  to: /path/to/rubygems
+  parallelism: 10
+  except:
+    - mail
+  only:
+    - rake
+    - rails
+```
+
+This will resolve all dependencies of `rails` and `rake` and sync all of them, but never sync `mail` (for example, if you ship your own version).
+
+This script syncs _all_ versions of the found gems.
+
+## Word of warning
+
+This does not supply a gem server. You will have to manage that server yourself.
+
+## License
+
+MIT, see `LICENSE.txt`
+
+## Thanks
+
+To the rubygems team for running rubygems and implementing rubygems-mirror in the first place.
 
 ## Contributing
 
--- lib/gem-sparse-mirror/command.rb
+++ lib/gem-sparse-mirror/command.rb
@@ -1,56 +1,56 @@
-require 'gem-sparse-mirror/mirror'
+# copied from rubygems-mirrors command.rb (MIT)
 
+require 'gem-sparse-mirror/mirror'
 module Gem
   module SparseMirror
     class Command
       SUPPORTS_INFO_SIGNAL = Signal.list['INFO']
-    
       def execute
         config_file = File.join Gem.user_home, '.gem', '.mirrorrc'
-    
+
         raise "Config file #{config_file} not found" unless File.exist? config_file
-    
+
         mirrors = YAML.load_file config_file
-    
+
         raise "Invalid config file #{config_file}" unless mirrors.respond_to? :each
-    
+
         mirrors.each do |mir|
           raise "mirror missing 'from' field" unless mir.has_key? 'from'
           raise "mirror missing 'to' field" unless mir.has_key? 'to'
-    
+
           get_from = mir['from']
           save_to = File.expand_path mir['to']
-    
+
           parallelism = mir['parallelism']
-    
+
           raise "Directory not found: #{save_to}" unless File.exist? save_to
           raise "Not a directory: #{save_to}" unless File.directory? save_to
-    
-          mirror = Gem::Sparse::Mirror::Mirror.new(get_from, save_to, parallelism)
+
+          mirror = Gem::SparseMirror::Mirror.new(get_from, save_to, parallelism)
           mirror.only = mir["only"]
           mirror.except = Array(mir["except"])
-    
+
           puts "Fetching: #{mirror.from(Gem::Mirror::SPECS_FILE_Z)} with #{parallelism} threads"
           mirror.update_specs
-    
+
           puts "Total gems: #{mirror.gems.size}"
-    
+
           num_to_fetch = mirror.gems_to_fetch.size
-    
+
           #progress = ui.progress_reporter num_to_fetch,
-#                                      "Fetching #{num_to_fetch} gems"
-    
+    #                                      "Fetching #{num_to_fetch} gems"
+
           trap(:INFO) { puts "Fetched: #{progress.count}/#{num_to_fetch}" } if SUPPORTS_INFO_SIGNAL
-    
+
           #mirror.update_gems { progress.updated true }
-    
+
           num_to_delete = mirror.gems_to_delete.size
-    
+
           #progress = ui.progress_reporter num_to_delete,
           #                           "Deleting #{num_to_delete} gems"
-    
+
           trap(:INFO) { puts "Fetched: #{progress.count}/#{num_to_delete}" } if SUPPORTS_INFO_SIGNAL
-    
+
           mirror.delete_gems { progress.updated true }
         end
       end
